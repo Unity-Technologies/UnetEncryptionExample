cccef97176717ab42a717d88a3387f16a0c53412
Peter Pimley <peter.pimley@unity3d.com>
2018-10-01 14:36:23 +0100

    Added example project

---
+++ ExampleProject/Assets/Client.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Linq;
+
+using System.Runtime.InteropServices;
+
+public class Client : MonoBehaviour {
+
+
+
+	[SerializeField]
+	private string m_serverAddr;
+
+	[SerializeField]
+	private int m_serverPort;
+
+
+	private int m_serverHost, m_connectionId;
+
+	public bool IsConnected { get; private set; }
+
+
+	[DllImport("UnetEncryption")]
+	private static extern int SetUuidForNextConnection(string uuid_str);
+
+
+
+
+	public void Setup (string key_uuid)
+	{
+		SetUuidForNextConnection(key_uuid);
+	}
+
+	// Use this for initialization
+	IEnumerator Start () {
+
+		ConnectionConfig config = new ConnectionConfig();
+
+		config.AddChannel(QosType.Unreliable);
+		config.PacketSize = 1000;
+
+		Debug.LogFormat("Default config has {0} channels", config.ChannelCount);
+		HostTopology top = new HostTopology(config, 10);
+
+		int serverHostId = NetworkTransport.AddHost(top);
+
+		byte err = 0;
+		Debug.LogFormat("Client connecting...");
+
+
+
+
+		NetworkTransport.Connect(serverHostId, m_serverAddr, m_serverPort, 0, out err);
+
+		StartCoroutine(SendReceive());
+		StartCoroutine(SendTime());
+
+		yield break;
+	}
+
+
+	IEnumerator SendReceive ()
+	{
+		byte[] buffer = new byte[(64 * 1024) - 1];
+
+
+		while (true) {
+
+			yield return null;
+
+			int host = 0;
+			int connection = 0;
+			int channel = 0;
+			int size = 0;
+			byte err = 0;
+
+			NetworkEventType evt = NetworkTransport.Receive(out host, out connection, out channel, buffer, buffer.Length, out size, out err);
+
+			if (err != 0) {
+				Debug.LogWarningFormat("SendReceive on client failed with {0}", err);
+				continue;
+			}
+
+			switch (evt) {
+
+				case NetworkEventType.ConnectEvent:
+					Debug.LogFormat("Connected as client with connection id {0}.  Server is host {1}", connection, host);
+					m_serverHost = host;
+					m_connectionId = connection;
+					IsConnected = true;
+					break;
+
+				case NetworkEventType.DisconnectEvent:
+					Debug.LogFormat("Lost connection to server.");
+					IsConnected = false;
+					break;
+
+				case NetworkEventType.DataEvent:
+					string s = System.Text.Encoding.UTF8.GetString(buffer, 0, size);
+					Debug.LogFormat(
+						"Client receives message from host {0} on connection {1} channel {2} said: \"{3}\"", host, connection, channel, s);
+					break;
+
+			}
+		}
+	}
+
+
+	IEnumerator SendTime ()
+	{
+		while (true) {
+			yield return new WaitForSeconds(5.0f);
+			if (!IsConnected)
+				continue;
+
+
+			System.DateTime now = System.DateTime.Now;
+
+			string s = string.Format("I am the client and I think the time is \"{0}\"", now);
+
+			byte[] buffer = System.Text.Encoding.UTF8.GetBytes(s);
+			byte err = 0;
+
+			NetworkTransport.Send(m_serverHost, m_connectionId, 0, buffer, buffer.Length, out err);
+
+			if (err != 0) {
+				Debug.LogWarning("Client failed to send time.");
+			}
+		}
+	}
+
+}
---
+++ ExampleProject/Assets/PluginInit.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Linq;
+
+using System.Runtime.InteropServices;
+using AOT;
+
+public class PluginInit : MonoBehaviour {
+
+
+	[SerializeField]
+	private int m_waitFrames;
+
+	[System.Serializable]
+	public class KeySet
+	{
+		public string uuid;
+		public string key;
+		public string iv;
+		public string hmac_key;
+	}
+
+	[SerializeField]
+	private KeySet[] m_keys;
+
+	public IEnumerable<KeySet> KeySets { get { return m_keys; } }
+
+
+	[DllImport("UnetEncryption")]
+	private static extern int UnetEncryptionInit();
+
+	[DllImport("UnetEncryption")]
+	private static extern int AddConnectionKeys(
+		string uuid_str,
+		byte[] encryption_key,
+		uint encryption_key_length,
+		byte[] hmac_key,
+		uint hmac_key_length,
+		byte[] iv,
+		uint iv_length);
+
+	[DllImport("UnetEncryption")]
+	private static extern int RemoveConnectionKeys(
+		string uuid_str);
+
+
+	private delegate void LogFunc(string s);
+
+	[DllImport("UnetEncryption")]
+	private static extern void SetLogFunc(LogFunc f);
+
+
+	private IEnumerator Start()
+	{
+		// e.g. let applogontext awake
+		for (int i = 0; i < m_waitFrames; ++i)
+			yield return null;
+
+
+		int err = 0;
+
+		err = UnetEncryptionInit();
+		Debug.Assert(err == 0);
+
+		SetLogFunc(LogCB);
+		Debug.Assert(err == 0);
+
+		foreach (KeySet k in KeySets) {
+			byte[] enc = System.Convert.FromBase64String(k.key);
+			byte[] iv = System.Convert.FromBase64String(k.iv);
+			byte[] hmac_key = System.Convert.FromBase64String(k.hmac_key);
+
+			Debug.LogFormat("Key {0} has hmac key {1}", k.uuid, string.Join(", ", hmac_key.Select(x => ((int)x).ToString("x")).ToArray()));
+
+			Debug.LogFormat("Lengths {0} {1} {2}", enc.Length, hmac_key.Length, iv.Length);
+
+			err = AddConnectionKeys(k.uuid, enc, (uint)enc.Length, hmac_key, (uint)hmac_key.Length, iv, (uint)iv.Length);
+			Debug.AssertFormat(err == 0, "Failed to add key: {0}", err);
+		}
+
+
+		NetworkTransport.Init();
+
+		string dll_ext = "";
+		string extra_path = "";
+		string lib_prefix = "";
+		RuntimePlatform platform = Application.platform;
+		switch (platform) {
+			case RuntimePlatform.LinuxEditor:
+			case RuntimePlatform.LinuxPlayer:
+				extra_path = "x86_64";
+				dll_ext = "so";
+				lib_prefix = "lib";
+				break;
+			case RuntimePlatform.WindowsEditor:
+			case RuntimePlatform.WindowsPlayer:
+			case RuntimePlatform.XboxOne:
+				dll_ext = "dll";
+				break;
+		}
+
+		string path = string.Format("{0}/Plugins/{1}/{2}UnetEncryption.{3}", Application.dataPath, extra_path, lib_prefix, dll_ext);
+
+		Debug.LogFormat("Attempting to load \"{0}\"", path);
+		
+		bool loadedOk = UnityEngine.Networking.NetworkTransport.LoadEncryptionLibrary(path);
+		Debug.Assert(loadedOk, "Failed to call LoadEncryptionLibrary.");
+
+		Debug.LogFormat("Plugin initted.");
+	}
+
+
+
+	private static List<string> s_lines = new List<string>();
+
+	[MonoPInvokeCallback(typeof(LogFunc))]
+	private static void LogCB (string s)
+	{
+		lock (s_lines) {
+			s_lines.Add(s);
+		}
+	}
+
+
+	private void Update()
+	{
+		lock (s_lines) {
+			foreach (string s in s_lines) {
+				Debug.LogFormat ("Native> {0}", s);
+			}
+			s_lines.Clear();
+		}
+	}
+
+}
